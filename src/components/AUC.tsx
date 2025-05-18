f43758cc7636770c9126a4c3d9e742a2cba7880b
islamhalim <islamhalim@yahoo.com>
2025-05-18 16:33:39 +0300

    Create AUC.tsx

---
+++ src/components/AUC.tsx
@@ -0,0 +1,51 @@
+
+import React from "react";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "./ImageGalleryStyles.css"; // Your custom file
+
+// Import all images
+import u0 from "./imgs/u0.png";
+import u1 from "./imgs/u1.png";
+import u2 from "./imgs/u2.png";
+import u3 from "./imgs/u3.png";
+import u4 from "./imgs/u4.png";
+import u5 from "./imgs/u5.png";
+import u6 from "./imgs/u6.png";
+import u7 from "./imgs/u7.png";
+import u8 from "./imgs/u8.png";
+import u9 from "./imgs/u9.png";
+import u10 from "./imgs/u10.png";
+
+
+// Add images to array
+const images = [
+  u0, u1, u2, u3, u4, u5, u6, u7, u8, u9, u10].map((img) => ({
+  original: img,
+  thumbnail: img,
+}));
+
+const Highlights: React.FC = () => {
+  return (
+    <section id="highlights" className="py-10 bg-white">
+       <div className="text-center mb-12">
+          <h2 className="text-2xl sm:text-4xl font-bold text-gray-900 mb-4">
+          AUC - 15th May 2025
+          </h2>
+       </div>
+
+      <div className="max-w-5xl mx-auto px-4">
+        <ImageGallery
+          items={images}
+          autoPlay={true}
+          slideInterval={4000}
+          showThumbnails={true}
+          showFullscreenButton={true}
+          showPlayButton={false}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
